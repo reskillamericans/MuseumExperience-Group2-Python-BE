5bd82adc943ee6c25432ed3d6740151fdfb007f0
mofresh27 <81204352+mofresh27@users.noreply.github.com>
2021-06-26 09:26:43 -0700

    Different types of user using AbstractUser

--- adminapp/admin.py
+++ adminapp/admin.py
@@ -1,3 +1,9 @@
+from adminapp.models import Exhibit, Exhibit_Notification, Question, User, User_Porfile
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Exhibit)
+admin.site.register(Question)
+admin.site.register(User_Porfile)
+admin.site.register(User)
+admin.site.register(Exhibit_Notification)
--- museumadmin/settings.py
+++ museumadmin/settings.py
@@ -125,3 +125,6 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 django_heroku.settings(locals())
+
+
+AUTH_USER_MODEL = 'adminapp.USER'

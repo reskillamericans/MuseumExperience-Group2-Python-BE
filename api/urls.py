e28a87e7e22f6d3dc001b2ab0a3b181eeaec6457
Oluwafemi Adenuga <phemmylintry@gmail.com>
2021-07-14 05:36:07 -0700

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -12,5 +12,5 @@ urlpatterns = [
     path('get-exhibits', api.views.ExhibitView.as_view(), name='get-exhibits'),
     url(r'^get-exhibits-details/(?P<uuid>.*)$', api.views.ExhibitDetail.as_view()),
     url(r'^create-user/$', api.views.CreateUserView.as_view()),
-    url('faqs', api.views.FaqView.as_view(), name='faq')
+    path('faqs', api.views.FaqView.as_view(), name='faq')
 ]
